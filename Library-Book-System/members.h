bddf5fb48d10aa68f72f43cf07726e61a9750664
WHitE-TITaN <dk488621@gmail.com>
2025-03-31 22:59:01 +0530

    added new member function

--- Library-Book-System/members.h
+++ Library-Book-System/members.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "book.h"
+#include <functional>
 
 class members{
 private:
     unordered_map<int, pair<string, tm>> allMembers;            //id - > {name , valid memebrship date ->>};
     time_t systemDay;
-    string date;
+    tm validTill;
+    string date;                                               //only to generate hash
 
 public:
     members();
@@ -16,26 +18,38 @@ public:
     bool isValidMember(string name);
 };
 
+
+//constructor ->>
 members::members()
 {
     systemDay = time(0);
+    validTill = *localtime(&systemDay);
     date = ctime(&systemDay);
 }
 
+
+// add new user function ->>
 bool members::registerUser(){
     string name;
     string rawInput;
     cin.ignore();
 
-    cout<<"\n\tName /-";
+    cout<<"\n\tName /- ";
     getline(cin, name);
 
     for(auto character : name){
         rawInput.push_back(character);
     }
-    for(auto chacarter : date){
-        
+    for(auto character : date){
+        rawInput.push_back(character);
     }
+    
+    validTill.tm_year = 1900 + validTill.tm_year;
+    validTill.tm_mon = validTill.tm_mon + 2;        // 1 to index it to 1 - january + 1 subscription
+
+    int hashValue = hash<string>{}(rawInput) % 1000000;
+    allMembers[hashValue].first = name;
+    allMembers[hashValue].second = validTill;
 }
 
 members::~members()
