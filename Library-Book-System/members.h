766aa161ceeb3172b0d8e0bd3f949ee615c8f562
WHitE-TITaN <dk488621@gmail.com>
2025-03-31 22:32:02 +0530

    added members hash id generation

--- Library-Book-System/members.h
+++ Library-Book-System/members.h
@@ -4,17 +4,38 @@
 
 class members{
 private:
-    unordered_map<string, pair<tm, tm>> allMembers;
+    unordered_map<int, pair<string, tm>> allMembers;            //id - > {name , valid memebrship date ->>};
+    time_t systemDay;
+    string date;
 
 public:
     members();
     ~members();
 
+    bool registerUser();
     bool isValidMember(string name);
 };
 
 members::members()
 {
+    systemDay = time(0);
+    date = ctime(&systemDay);
+}
+
+bool members::registerUser(){
+    string name;
+    string rawInput;
+    cin.ignore();
+
+    cout<<"\n\tName /-";
+    getline(cin, name);
+
+    for(auto character : name){
+        rawInput.push_back(character);
+    }
+    for(auto chacarter : date){
+        
+    }
 }
 
 members::~members()
