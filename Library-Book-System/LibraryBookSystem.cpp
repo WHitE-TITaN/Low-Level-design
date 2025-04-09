6371baf982ab8f4ee72a68c985959e9e6fc3c386
WHitE-TITaN <dk488621@gmail.com>
2025-04-09 22:24:49 +0530

    adding main Diver code

--- Library-Book-System/LibraryBookSystem.cpp
+++ Library-Book-System/LibraryBookSystem.cpp
@@ -1,9 +1,36 @@
 #include "members.h"
+#include "informationWareHouse.h"
 
 using namespace std;
 
 int main(){
-    members newmember;
-    newmember.registerUser();
-    newmember.isValidMember(1);
+    bool SystemOnline = true;
+
+    while(SystemOnline){
+        string Operation;
+        cout<<"no. function command\n";
+        cout<<"1.  add user  /- /au\n"<<
+        "2. add book /- /ab\n"<<
+        "3. issue Book /- /ib\n"<<
+        "4. exit /- /exit";
+
+        cin.ignore();
+        getline(cin, Operation);
+
+        if(Operation == "/exit"){
+            cout<<" -- Have a nice day -- ";
+            SystemOnline = false;
+
+            return 0;
+        }
+
+        if(Operation == "/au"){
+            members *newUser = new members();
+            newUser->registerUser();
+        }
+
+        if(Operation == "/ab"){
+        
+        }
+    }
 }
--- Library-Book-System/informationWareHouse.h
+++ Library-Book-System/informationWareHouse.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "book.h"
 
 class informationWareHouse
