14706766b5685f7c8a5155d0fdbba990ca2ebeb4
WHitE-TITaN <dk488621@gmail.com>
2025-04-18 22:07:42 +0530

    reading hall check in function

--- Library-Book-System/inLibrary.h
+++ Library-Book-System/inLibrary.h
@@ -1,2 +1,15 @@
 #include<members.h>
 
+class readingHall{
+    unordered_set<int> membersIn;
+    public:
+    bool checkIN(int id){
+        members *checkingIN;
+        if(!checkingIN->isValidMember(id)){
+            return false;
+        }
+        cout<<"\n*Welocme Sir*";
+        membersIn.insert(id);
+        return true;
+    }
+};
